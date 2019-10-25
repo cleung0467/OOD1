433e7d5b2ccbf14c90e45be43b28960390b01e50
Cynthia Leung <46499193+gyleung01@users.noreply.github.com>
2019-10-25 02:31:30 -0400

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,8 +1,10 @@
 package project;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 
+import java.math.BigDecimal; //Use BigDecimal 
+import java.util.ArrayList;  //Use ArrayList 
+
+/** Company inherits from Customer class */
 public class Company extends Customer {
 	Company() {
 
