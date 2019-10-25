552ed3d25fea0a52deda4c1c61a46f9c4270a76b
Cynthia Leung <46499193+gyleung01@users.noreply.github.com>
2019-10-25 02:26:50 -0400

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,4 @@
+/** This is a bank account. You are able to retrieve your account balance, deposit money and withdraw money.*/
 package project;
 
 import java.math.BigDecimal;
@@ -7,11 +8,13 @@ public class BankAccount {
 	private static long nextAccountID = 1000;
 	private static BigDecimal balance;
 
+/** This is the auto-generator for account ID's. It does not store anything and creates new variables every time the program runs.*/	
 	public static long makeAccountID() {
 		nextAccountID += 5;
 		return nextAccountID;
 	}
-  
+	
+/** Get balance */
   	private BigDecimal getBalance() {
 		return balance;
 	}
@@ -19,7 +22,8 @@ public class BankAccount {
 	public void getBalance(BigDecimal balance) {
 		this.balance = balance;
 	}
-
+	
+/** Deposit money and adds to the original balance */
 	public static BigDecimal deposit(BigDecimal depositAmt) {
 		balance = depositAmt.add(depositAmt);
 		return balance;
@@ -30,5 +34,6 @@ public class BankAccount {
 		return balance;
 	}
 
+/** Array list */
 	ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
 }
