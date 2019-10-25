70897a35848105bbfd7c183d517ec3e5dcb1e804
Cynthia Leung <46499193+gyleung01@users.noreply.github.com>
2019-10-25 02:39:17 -0400

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,18 +1,22 @@
-  package project;
+package project;
 
-import java.math.BigDecimal;
+import java.math.BigDecimal; //Use BigDecimal
 
+//Savings inherits from BankAccount
 public class Savings extends BankAccount {
 	Savings() {
 
 	}
 
+//Set type of account
       public String setType(String accountType){
       return accountType;
     }
   
+//Initiate interest rate to O
 	BigDecimal interestRate = new BigDecimal(0);
 
+//Get and Set interest rate
 	private BigDecimal getInterestRate() {
 		return interestRate;
 	}
