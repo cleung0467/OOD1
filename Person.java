2ef6164634ce730cdb4831533c1be85a738f491b
Cynthia Leung <46499193+gyleung01@users.noreply.github.com>
2019-10-25 02:37:44 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,7 +1,7 @@
 package project;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.math.BigDecimal; //Use BigDecimal
+import java.util.ArrayList;  //Use ArrayList
 
 /*Person inherits from Customer */
 public class Person extends Customer {
