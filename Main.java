0da4b1c57b6490aa500b8df6016860e91d176e12
Cynthia Leung <46499193+gyleung01@users.noreply.github.com>
2019-10-24 23:14:52 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,28 +4,20 @@ import java.math.BigDecimal;
 
 public class Main {
 	public static void main(String[] args) {
-		BankAccount checking = new BankAccount();
-		Checking.setType();
-		Checking.setBalance(0);
-
-		BankAccount Savings = new BankAccount();
-		Savings.setType("Savings");
-		Savings.setBalance(0);
-		Savings.setInterestRate();
 		
+        BankAccount checking = new BankAccount();
+		BankAccount Savings = new BankAccount();
 		BigDecimal balance = new BigDecimal(1_000_000);
 		long nextAccountID = 150_000;
 		BigDecimal depositAmt = new BigDecimal(1_234_235);
 		BigDecimal withdrawAmt = new BigDecimal(50);
 		BigDecimal setBalance = new BigDecimal(0);
-
+        BigDecimal setInterestRate = new BigDecimal(0);
+      
 		System.out.println("The Account ID is " + nextAccountID);
 		System.out.println("Your balance is " + balance);
 		System.out.println("You deposited " + depositAmt);
-		System.out.println("Your balance is now " + balance);
 		System.out.println("You withdrew " + withdrawAmt);
-		System.out.println("Your balance is now " + balance);
-		System.out.println("You set your balance as " + setBalance);
-		System.out.println("Your balance is now " + balance);
+		System.out.println("You reset your balance as " + setBalance);
 	}
 }
