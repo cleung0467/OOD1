be963dd7b3109ad1ca75200dd8bb0be01b55be50
Cynthia Leung <46499193+gyleung01@users.noreply.github.com>
2019-10-24 23:29:11 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,9 +16,9 @@ public class Main {
       
       	System.out.println("Your Bank Account ID is " + BankAccount.makeAccountID());
 		System.out.println("The User ID is " + nextAccountID);
-		System.out.println("Your balance is " + balance);
-		System.out.println("You deposited " + depositAmt);
-		System.out.println("You withdrew " + withdrawAmt);
-		System.out.println("You reset your balance as " + setBalance);
+		System.out.println("Your balance is " + balance  + "$");
+		System.out.println("You deposited " + depositAmt  + "$");
+		System.out.println("You withdrew " + withdrawAmt  + "$");
+		System.out.println("You reset your balance to " + setBalance + "$");
 	}
 }
