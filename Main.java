014cad96646e18e26e5c8849ed6d4cb1d3b20df7
Cynthia Leung <46499193+gyleung01@users.noreply.github.com>
2019-10-25 02:36:59 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,29 @@
 package project;
 
-import java.math.BigDecimal;
+import java.math.BigDecimal; //Use BigDecimal
 
 public class Main {
 	public static void main(String[] args) {
-		
-        BankAccount checking = new BankAccount();
-		BankAccount Savings = new BankAccount();
-		BigDecimal balance = new BigDecimal(1_000_000);
-		long nextAccountID = 150_000;
-		BigDecimal depositAmt = new BigDecimal(1_234_235);
-		BigDecimal withdrawAmt = new BigDecimal(50);
-		BigDecimal setBalance = new BigDecimal(0);
-        BigDecimal setInterestRate = new BigDecimal(0);
+
+/** Call To methods */
+                BankAccount checking = new BankAccount(); //Make checking account
+		BankAccount Savings = new BankAccount();  //Make savings account
+		BigDecimal balance = new BigDecimal(1_000_000);  //Get Balance
+		long nextAccountID = 150_000;  //get Account ID
+		BigDecimal depositAmt = new BigDecimal(1_234_235); //Get deposit amount
+		BigDecimal withdrawAmt = new BigDecimal(50);  //Get withdrawl amount
+		BigDecimal setBalance = new BigDecimal(0); //Set balance to any number
+        BigDecimal setInterestRate = new BigDecimal(0); //Set interest rate
       
-      	System.out.println("Your Bank Account ID is " + BankAccount.makeAccountID());
+/** Print out Call To's */		
+      		System.out.println("Your Bank Account ID is " + BankAccount.makeAccountID()); 
 		System.out.println("The User ID is " + nextAccountID);
 		System.out.println("Your balance is " + "$" + balance);
 		System.out.println("You deposited " + "$" + depositAmt);
 		System.out.println("You withdrew " + "$" + withdrawAmt);
 		System.out.println("You reset your balance to " + "$" + setBalance);
-        System.out.println("The interest rate on your savings account is " + setInterestRate + "%");
-        System.out.println(Checking.firstCheckIssued());  
-        System.out.println(Checking.getFirstCheck());
+       	 	System.out.println("The interest rate on your savings account is " + setInterestRate + "%");
+		System.out.println(Checking.firstCheckIssued());  
+		System.out.println(Checking.getFirstCheck());
 	}
 }
