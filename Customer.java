24b7ff9efdb8a3ad9d0199990cad6bfefb2c5d83
Cynthia Leung <46499193+gyleung01@users.noreply.github.com>
2019-10-25 02:32:42 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,18 +1,20 @@
-
 package project;
 
-import java.util.ArrayList;
+import java.util.ArrayList; //Use ArrayList
 
+/** Customer inherits from BankAccount class */
 public abstract class Customer extends BankAccount {
 	Customer() {
 
 	}
 
+//Create variables
 	public static long nextCustomerID = 2_000_000;
 	private String name;
 	private String address;
 	private long taxIDNumber;
 
+//Automatically generate customerID's
 	public long customerID() {
 		nextCustomerID += 7;
 		return nextCustomerID;
